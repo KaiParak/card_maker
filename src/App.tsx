676f508075b6d5b5b233f776e7a8ef49c2d760ec
Alexey <kraehg@yandex.com>
2024-01-02 00:18:59 +0300

    lab2 описал нужные типы

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,92 @@
 import React from 'react';
+import {ArtType, CardDataType, ImageType, TextType} from "./types/types";
 
-function App() {
+const App = () => {
+    const textObject: TextType = {
+        id: "text",
+        isSelected: true,
+        width: 100,
+        height: 100,
+        x: 10,
+        y: 10,
+        content: "ABCD",
+        fontFamily: "Arial",
+        fontSize: 18,
+        fontColor: "red",
+        decorations: ['bold', 'italic', 'underline']
+    }
+
+    const imageObject: ImageType = {
+        id: "image",
+        width: 400,
+        height: 600,
+        x: 300,
+        y: 400,
+        isSelected: false,
+        imageSrc: ""
+    }
+
+    const artObject: ArtType = {
+        id: "image",
+        width: 400,
+        height: 600,
+        x: 300,
+        y: 400,
+        isSelected: false,
+        artSrc: ""
+    }
+
+    // Минимальные данные. Коллекции пустые.
+    const cardData1: CardDataType = {
+        filter: {
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 0
+        },
+        canvas: {
+            width: 0,
+            height: 0
+        },
+        objects: []
+    }
+
+    // В коллекциях могут быть некоторые элементы (не все типы элементов могут быть).
+    const cardData2: CardDataType = {
+        filter: {
+            r: 255,
+            g: 255,
+            b: 255,
+            a: 255
+        },
+        canvas: {
+            width: 800,
+            height: 600
+        },
+        objects: [
+            textObject
+        ]
+    }
+
+    // Максимальные данные. Все коллекции заполнены всеми типами данных.
+    const cardData3: CardDataType = {
+        filter: {
+            r: 255,
+            g: 255,
+            b: 255,
+            a: 255
+        },
+        canvas: {
+            width: 800,
+            height: 600
+        },
+        objects: [
+            textObject,
+            imageObject,
+            artObject
+        ]
+    }
+
     return (
         <div>
             My app
---
+++ src/types/types.ts
@@ -0,0 +1,60 @@
+type SizeType = {
+    width: number,
+    height: number
+}
+
+type PositionType = {
+    x: number,
+    y: number
+}
+
+type BoundingBoxType = SizeType & PositionType
+
+type BaseElementType = BoundingBoxType & {
+    id: string,
+    isSelected: boolean
+}
+
+type ImageType = BaseElementType & {
+    imageSrc: string
+}
+
+type TextDecorationType = 'italic' | 'bold' | 'underline'
+
+type TextType = BaseElementType & {
+    content: string,
+    decorations: TextDecorationType[],
+    fontSize: number,
+    fontFamily: string,
+    fontColor: string
+}
+
+type ArtType = BaseElementType & {
+    artSrc: string
+}
+
+type ObjectType = ImageType | TextType | ArtType
+
+type FilterType = {
+    r: number,
+    g: number,
+    b: number,
+    a: number
+}
+
+type CanvasType = SizeType
+
+type CardDataType = {
+    filter: FilterType,
+    canvas: CanvasType,
+    objects: ObjectType[]
+}
+
+export type {
+    ImageType,
+    TextType,
+    ArtType,
+    FilterType,
+    CanvasType,
+    CardDataType
+}
